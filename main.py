768f4163fb3143c25b121707e663ecd417aa5f2c
Booingy-Droid <129057454+Booingy-Droid@users.noreply.github.com>
2023-04-09 09:57:08 +0200

    main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import string
+import random
+
+length = 19
+amount_of_codes = int(input("How many codes do you need? "))
+lower = string.ascii_lowercase
+upper = string.ascii_uppercase
+num = string.digits
+
+combined = lower + upper + num
+
+for i in range(amount_of_codes):
+  temp = random.sample(combined,length)
+  password = "".join(temp)
+  print(password)
